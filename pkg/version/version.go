0e492ea43768f0f2bb4add87876d206b2203a8b7
cgil <lao.tseu.is.alive@gmail.com>
2025-07-15 11:13:19 +0200

    corrected bug in baseUrl

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goEmployeJwt"
 	AppSnake   = "go-cloud-k8s-employe-jwt"
-	VERSION    = "0.0.7"
+	VERSION    = "0.0.8"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-employe-jwt"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
